d3d869074e873fc909cbcb78a35e11c3e0f5d4dd
Felipe Santana <santanafelipe.dev@gmail.com>
2022-09-23 13:25:06 -0300

    Alterando textos e valores

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,6 +12,17 @@ new Splide('#clientes_slider', {
     type: 'loop',
     pagination: false,
     perPage: 7,
+    breakpoints: {
+        992: {
+            perPage: 5
+        },
+        768: {
+            perPage: 4
+        },
+        560: {
+            perPage: 3
+        }
+    },
     autoplay: true,
     speed: 3000,
     arrows: false
